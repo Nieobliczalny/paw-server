9d640debb46b16b3282657f2d84c3e25d990d1d3
Nieobliczalny <krychu1993@gmail.com>
2016-10-12 22:34:01 +0200

    Dodanie mockupu API (GET dla tablicy, listy, zadan, zalogowany user)

--- src/Example/SampleBundle/Controller/DefaultController.php
+++ src/Example/SampleBundle/Controller/DefaultController.php
@@ -15,4 +15,59 @@ class DefaultController extends FOSRestController
 		$view = $this->view($data,200);
 		return $this->handleView($view);
     }
+    public function getBoardsAction()
+    {
+        $data = [[
+			'id' => '1',
+			'name' => 'Board 1',
+		]];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
+    public function getBoardAction($id)
+    {
+        $data = [
+			'id' => '1',
+			'name' => 'Board 1',
+		];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
+    public function getBoardListsAction($id)
+    {
+		//TODO: $id określa tablicę z której listę pobrać
+        $data = [[
+			'name' => 'List 1',
+		],[
+			'name' => 'List 2',
+		]];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
+    public function getBoardListAction($id, $list)
+    {
+		//TODO: $id określa tablicę z której listę pobrać
+		if ($list == 1) $data = [ 'name' => 'List 1' ];
+		else if ($list == 2) $data = [ 'name' => 'List 2' ];
+		else $data = [];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
+    public function getBoardListTasksAction($id, $list)
+    {
+		//TODO: $id określa tablicę z której listę pobrać
+		if ($list == 1) $data = [['name' => 'Task 1'], ['name' => 'Task 2']];
+		else if ($list == 2) $data = [['name' => 'Task 1'], ['name' => 'Task 2'], ['name' => 'Task 3'], ['name' => 'Task 4']];
+		else $data = [];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
+    public function getLoggedUserAction()
+    {
+        $data = [
+			'name' => 'User001',
+		];
+		$view = $this->view($data,200);
+		return $this->handleView($view);
+    }
 }
